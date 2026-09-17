f07a507becfac2d7df4561211c9c612d69982b95
Nurilya97 <nurisauranbayeva@gmail.com>
2026-09-17 14:23:09 +0500

    Apply Apple-style typography overrides

--- src/spatial.jsx
+++ src/spatial.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ImmersiveUniverse } from './components/ImmersiveUniverse.jsx'
+import './components/Typography.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ImmersiveUniverse />
   </StrictMode>,
 )
-
