9ba1ff967ff560de5d52554c7ef5dcedc1c1d21e
Nurilya97 <nurisauranbayeva@gmail.com>
2026-09-17 17:31:18 +0500

    Load circular word orbit styles

--- src/spatial.jsx
+++ src/spatial.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ImmersiveUniverse } from './components/ImmersiveUniverse.jsx'
 import './components/Typography.css'
+import './components/WordOrbit.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
