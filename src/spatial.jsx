92959eef18149b612c4dd491fdc0c44237d37aef
Nurilya97 <nurisauranbayeva@gmail.com>
2026-09-26 11:42:00 +0500

    Load Composition spatial flow styling

--- src/spatial.jsx
+++ src/spatial.jsx
@@ -5,6 +5,7 @@ import { ImmersiveUniverse } from './components/ImmersiveUniverse.jsx'
 import { AyahInlineMeaningLayer } from './components/AyahInlineMeaningLayer.jsx'
 import './components/Typography.css'
 import './components/WordOrbit.css'
+import './components/CompositionImmersiveFlow.css'
 
 const BUILD_SHA = import.meta.env.VITE_BUILD_SHA || ''
 
